5f479f1acc203fa0528d76fd926a6348df2203e7
Altair97 <32910284+Altair97@users.noreply.github.com>
2019-08-06 16:06:11 +0200

    client update

--- SEP2_RE_Code/src/Client/Client.java
+++ SEP2_RE_Code/src/Client/Client.java
@@ -1,4 +1,6 @@
-package domain.mediator.Employee;
+package client;
+import domain.mediator.Employee.EmployeeManagerInterface;
+import domain.mediator.Shifts.ShiftManagerInterface;
 
 import domain.model.employees.Employee;
 
@@ -10,6 +12,8 @@ import java.util.ArrayList;
 public class Client {
 
 
+    //----------------------------------------EMPLOYEE-----------------------------------------------
+
     public Client() throws Exception {
 
         Registry registry = LocateRegistry.getRegistry(1098);
@@ -19,23 +23,35 @@ public class Client {
     public void addEmployee(int employeeId, String firstName, String lastName, String email) throws Exception {
 
 
-        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://192.168.1.108:1098/addEmployee");
+        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://192.168.43.80:1098/addEmployee");
         stub.addEmployee(employeeId, firstName, lastName,email);
 
     }
 
     public ArrayList<Employee> getEmployees() throws Exception
     {
-        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://192.168.1.108:1098/getEmployees");
+        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://192.168.43.80:1098/getEmployees");
         ArrayList<Employee> list = stub.getEmployees();
         return list;
     }
 
     public void removeEmployee(int id) throws Exception
     {
-        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://192.168.1.108:1098/removeEmployee");
+        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://192.168.43.80:1098/removeEmployee");
         stub.removeEmployee(id);
     }
+
+    //-------------------------------SHIFTS------------------------------------------------------------------
+
+    public void assignShift(int shiftId, int employeeId, int date, int startTime, int endTime, String task) throws Exception
+    {
+        ShiftManagerInterface stub = (ShiftManagerInterface) Naming.lookup("rmi://192.168.43.80:1098/getEmployees");
+        stub.assignShift(shiftId, employeeId, date, startTime, endTime, task);
+    }
+
+
+
+
 
 
 
