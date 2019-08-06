20ac63afaeeb5e4118da62bd6a8764cf91f61b13
ronald <ronaldhej@gmail.com>
2019-08-06 17:43:08 +0200

    re added module info

---
+++ SEP2_RE_Code/src/module-info.java
@@ -0,0 +1,35 @@
+/**
+ *
+ */
+/**
+ * @author Balkis
+ *
+ */
+module SEP2_RE {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires java.sql;
+
+    exports main to javafx.graphics;
+    exports controller.admin to javafx.fxml;
+    exports view.admin.mainAdmin to javafx.fxml;
+    exports view.admin.addUser to javafx.fxml;
+    exports view.admin.editUser to javafx.fxml;
+    exports view.admin.removeUser to javafx.fxml;
+    exports view.login to javafx.fxml;
+    exports view.user.mainUser to javafx.fxml;
+    exports view.admin.dashBoard to javafx.fxml;
+    exports view.user.dashBoard to javafx.fxml;
+
+    opens main to javafx.fxml;
+    opens view.login to javafx.fxml;
+    opens view.admin.addUser to javafx.fxml;
+    opens view.admin.editUser to javafx.fxml;
+    opens view.admin.removeUser to javafx.fxml;
+    opens view.admin.mainAdmin to javafx.fxml;
+    opens view.user.mainUser to javafx.fxml;
+    opens view.admin.dashBoard to javafx.fxml;
+    opens view.user.dashBoard to javafx.fxml;
+
+}
