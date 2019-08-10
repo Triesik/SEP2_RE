9dd49ea9a8bd901938b514a9e5f9d95f7bc30101
Altair97 <32910284+Altair97@users.noreply.github.com>
2019-08-10 22:06:15 +0200

    add employee in client

--- SEP2_RE_Code/src/client/Client.java
+++ SEP2_RE_Code/src/client/Client.java
@@ -16,7 +16,7 @@ import java.util.Calendar;
 public class Client {
 
 
-    //----------------------------------------EMPLOYEE-----------------------------------------------
+    //----------------------------------------EMPLOYEE-PORT: 1097----------------------------------------------
 
     public Client() throws Exception {
 
@@ -24,41 +24,57 @@ public class Client {
 
     }
 
-    public void addEmployee(int employeeId, String firstName, String lastName, String email) throws Exception {
+    public void addEmployee(int employeeId, String firstName, String lastName, String email, String userType) throws Exception {
 
 
-        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://192.168.1.98:1097/addEmployee");
-        stub.addEmployee(employeeId, firstName, lastName,email);
+        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://localhost:1097/addEmployee");
+        stub.addEmployee(employeeId, firstName, lastName,email, userType);
 
     }
 
     public ArrayList<Employee> getEmployees() throws Exception
     {
-        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://192.168.1.98:1097/getEmployees");
+        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://localhost:1097/getEmployees");
         ArrayList<Employee> list = stub.getEmployees();
         return list;
     }
 
     public void removeEmployee(int id) throws Exception
     {
-        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://192.168.1.98:1097/removeEmployee");
+        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://localhost:1097/removeEmployee");
         stub.removeEmployee(id);
     }
 
-    //-------------------------------SHIFTS------------------------------------------------------------------
-
-    public void assignShift(int shiftId, int employeeId, int date, int startTime, int endTime, String task) throws Exception
+    public boolean verifyPassword(int employeeId, String inputPassword) throws Exception
     {
-        ShiftManagerInterface stub = (ShiftManagerInterface) Naming.lookup("rmi://192.168.1.98:1099/assignShift");
+        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://localhost:1097/verifyPassword");
+        boolean verification = stub.verifyPassword(employeeId, inputPassword);
+        return verification;
+
+    }
+
+    public boolean isAdmin(int employeeId) throws Exception
+    {
+        EmployeeManagerInterface stub = (EmployeeManagerInterface) Naming.lookup("rmi://localhost:1097/isAdmin");
+        boolean check = stub.isAdmin(employeeId);
+        return check;
+    }
+
+    //-------------------------------SHIFTS--PORT:1099----------------------------------------------------------------
+
+    public void assignShift(int shiftId, int employeeId, String date, int startTime, int endTime, String task) throws Exception
+    {
+        ShiftManagerInterface stub = (ShiftManagerInterface) Naming.lookup("rmi://localhost:1099/assignShift");
         stub.assignShift(shiftId, employeeId, date, startTime, endTime, task);
     }
 
     public ArrayList<Shift> getWeekPlan(int employeeId, Calendar date) throws Exception
     {
-        ShiftManagerInterface stub = (ShiftManagerInterface) Naming.lookup("rmi://192.168.1.98:1099/getWeekPlan");
+        ShiftManagerInterface stub = (ShiftManagerInterface) Naming.lookup("rmi://localhost:1099/getWeekPlan");
         ArrayList<Shift> list = stub.getWeekPlan(employeeId, date);
         return list;
     }
+
 
 
 
