6ee554d7ebc47f35050490e393a833371fbb2a39
Altair97 <32910284+Altair97@users.noreply.github.com>
2019-08-13 22:02:39 +0200

    fix assign shift

--- SEP2_RE_Code/src/domain/mediator/shift/test.java
+++ SEP2_RE_Code/src/domain/mediator/shift/test.java
@@ -7,7 +7,7 @@ public class test {
     public static void main(String args[]) throws Exception {
 
         Client client = new Client();
-        client.checkIn(1);
+        client.assignShift(1, "2019-05-06", "15:30", "13:22", "dunno");
 
 
 
